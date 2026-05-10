a971f1302a230af91d6c10e7e89a25c10061a300
ActuallyMadhav <madhavra.2004@gmail.com>
2026-05-11 01:59:31 +0800

    can draw a chessboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include "raylib.h"
+#define BOARD_SIZE 8
+#define SQUARE_SIZE 60
+
+void drawBoard(){
+    for(int i = 0; i < BOARD_SIZE; i++){
+        for(int j = 0; j < BOARD_SIZE; j++){
+            Color squareColor = ((i + j) % 2) ? BLACK : WHITE;
+            DrawRectangle(i * SQUARE_SIZE, j * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE, squareColor);
+        }
+    }
+}
 
 int main(){
 
-    InitWindow(600, 600, "test");
+    InitWindow(BOARD_SIZE * SQUARE_SIZE, BOARD_SIZE * SQUARE_SIZE, "Chess Engine");
     SetTargetFPS(60);
 
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(RAYWHITE);
-
-        DrawRectangle(200, 150, 100, 100, BLACK);
+        drawBoard();
         EndDrawing();
     }
 
