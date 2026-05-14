15e1a516979a77c26a8f0a7b0e415baa731d53cd
ActuallyMadhav <madhavra.2004@gmail.com>
2026-05-15 01:03:57 +0800

    changed structure and implementations in board class. added board state tracker and move struct + methods. will continue implementation tomorrow

--- inc/board.h
+++ inc/board.h
@@ -6,13 +6,39 @@
 enum Piece{
     wpawn = 0, wknight = 1, wbishop = 2, wrook = 3, wqueen = 4, wking = 5,
     bpawn = 6, bknight = 7, bbishop = 8, brook = 9, bqueen = 10, bking = 11,
-    empty = 12
+    empty = 12, white = 13, black = 14
+};
+
+struct boardState{
+    // bitboards for each piece type
+    uint64_t bitboards[12];
+    // bitboard for en-passant
+    uint64_t enPassantTarget;
+
+    // castling availability
+    bool whiteKingCastle;
+    bool whiteQueenCastle;
+    bool blackKingCastle;
+    bool blackQueenCastle;
+
+    // white or black turn
+    uint8_t turn;
+
+};
+
+struct Move{
+    // origin and destination square
+    uint8_t fromSquare;
+    uint8_t toSquare;
+
+    // promotion validity;
+    uint8_t promote;
 };
 
 class Board{
 private:
-    uint64_t bitboards[12]; // one bitboard for each piece type. 1 - piece on square, 0 - piece not on square
-
+    boardState* stateTracker;   // array of board states to track game
+    int trackerIndex;
 public:
 
     Texture2D pieceSprites[12];
@@ -20,6 +46,10 @@ public:
     uint8_t getPiece(uint8_t square);
     void setPiece(uint8_t square, uint8_t piece);
     void loadSprites();
+
+    // move methods
+    void move(const Move& move);
+    void undo();
 
     // constructor
     Board();
--- src/board.cpp
+++ src/board.cpp
@@ -3,7 +3,7 @@
 uint8_t Board::getPiece(uint8_t square){
     uint64_t mask = uint64_t(1) << square;
     for(int i = 0; i < 12; i++){
-        if(bitboards[i] & mask){
+        if(stateTracker[trackerIndex].bitboards[i] & mask){
             return i;
         }
     }
@@ -14,10 +14,10 @@ void Board::setPiece(uint8_t square, uint8_t piece){
     uint64_t mask = uint64_t(1) << square;
     for(int i = 0; i < 12; i++){
         if(i == piece){
-            bitboards[i] |= mask;   // set bit
+            stateTracker[trackerIndex].bitboards[i] |= mask;   // set bit
         }
         else{
-            bitboards[i] &= ~mask;  // clear bit
+            stateTracker[trackerIndex].bitboards[i] &= ~mask;  // clear bit
         }
     }
 }
@@ -60,23 +60,77 @@ void Board::loadSprites(){
     SetTextureFilter(pieceSprites[wpawn], TEXTURE_FILTER_POINT);
 }
 
+void Board::move(const Move& move){
+    // add new state to state tracker
+    stateTracker[trackerIndex + 1] = stateTracker[trackerIndex++];
+
+    // to and from bitboards
+    uint64_t fromBitboard = uint64_t(1) << move.fromSquare;
+    uint64_t toBitboard = uint64_t(1) << move.toSquare;
+    uint64_t moveBitboard = fromBitboard | toBitboard;
+
+    // to move a piece
+    uint8_t movedPiece = empty;
+    int i, limit;
+    if(stateTracker[trackerIndex].turn == white){
+        i = 0;
+        limit = 6;
+    }
+    else{
+        i = 6;
+        limit = 12;
+    }
+
+    for(i; i < limit; i++){
+        if(stateTracker[trackerIndex].bitboards[i] & fromBitboard){
+            stateTracker[trackerIndex].bitboards[i] ^= moveBitboard;
+            movedPiece = i;
+            break;  // continue from here
+        }
+    }
+}
+
+void Board::undo(){
+    if(trackerIndex){
+        trackerIndex--;
+    }
+}
+
 Board::Board(){
-    bitboards[bpawn] = 0x000000000000ff00;
-    bitboards[bknight] = 0x0000000000000042;
-    bitboards[bbishop] = 0x0000000000000024;
-    bitboards[brook] = 0x0000000000000081;
-    bitboards[bqueen] = 0x0000000000000008;
-    bitboards[bking] = 0x0000000000000010;
-    bitboards[wpawn] = 0x00ff000000000000;
-    bitboards[wknight] = 0x4200000000000000;
-    bitboards[wbishop] = 0x2400000000000000;
-    bitboards[wrook] = 0x8100000000000000;
-    bitboards[wqueen] = 0x0800000000000000;
-    bitboards[wking] = 0x1000000000000000;
+    // allocating memory for entire game
+    stateTracker = new  boardState[1000];
+    trackerIndex = 0;
+    // starting positions
+    stateTracker[0].bitboards[bpawn] = 0x000000000000ff00;
+    stateTracker[0].bitboards[bknight] = 0x0000000000000042;
+    stateTracker[0].bitboards[bbishop] = 0x0000000000000024;
+    stateTracker[0].bitboards[brook] = 0x0000000000000081;
+    stateTracker[0].bitboards[bqueen] = 0x0000000000000008;
+    stateTracker[0].bitboards[bking] = 0x0000000000000010;
+    stateTracker[0].bitboards[wpawn] = 0x00ff000000000000;
+    stateTracker[0].bitboards[wknight] = 0x4200000000000000;
+    stateTracker[0].bitboards[wbishop] = 0x2400000000000000;
+    stateTracker[0].bitboards[wrook] = 0x8100000000000000;
+    stateTracker[0].bitboards[wqueen] = 0x0800000000000000;
+    stateTracker[0].bitboards[wking] = 0x1000000000000000;
+
+    // castling rights
+    stateTracker[0].whiteKingCastle = true;
+    stateTracker[0].whiteQueenCastle = true;
+    stateTracker[0].blackKingCastle = true;
+    stateTracker[0].blackQueenCastle = true;
+
+    // en passant bitboard
+    stateTracker[0].enPassantTarget = 0;
+
+    // white moves first
+    stateTracker[0].turn = white;
 }
 
 Board::~Board(){
     for(int i = 0; i < 12; i++){
         UnloadTexture(pieceSprites[i]);
     }
+
+    delete[] stateTracker;
 }
